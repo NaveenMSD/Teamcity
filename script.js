00cdf6e656dc225ef93285806c8c71ce0bb4327d
NaveenMSD <95711085+NaveenMSD@users.noreply.github.com>
2024-05-14 15:01:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+function addToScreen(value) {
+    document.getElementById('screen').value += value;
+}
+
+function clearScreen() {
+    document.getElementById('screen').value = '';
+}
